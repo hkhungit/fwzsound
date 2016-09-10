94b7237d6c1872e8426e3fe12634dcd084f8bb68
Jim Fwz <jim@futureworkz.com>
2016-09-10 12:37:05 +0700

    update android setup

--- android/app/src/main/java/com/fwzsound/MainActivity.java
+++ android/app/src/main/java/com/fwzsound/MainActivity.java
@@ -2,6 +2,10 @@ package com.fwzsound;
 
 import com.facebook.react.ReactActivity;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+import android.os.Bundle;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -12,5 +16,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "FwzSound";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      Fabric.with(this, new Crashlytics());
     }
 }
